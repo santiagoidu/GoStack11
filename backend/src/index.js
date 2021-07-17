7ac1e689b32ad1ad41aed50204b3259df008fa1b
santiagoidu <sr_santiago@icloud.com>
2021-07-17 18:21:54 -0300

    Back-end Node.js

---
+++ backend/src/index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+/* 
+Métodos HTTP:
+
+GET: Buscar informações
+POST: criar uma informação no back-end
+PUT/PATCH: Alterar uma informação no back-end
+DELETE: Deletar uma informação no back-end
+
+Tipos de parâmetros:
+
+Query Parans: Filtros e paginação
+Routes Parans: Identificar recursos (Atualizar/Deletar)
+Request Body: Conteúdo na hora criar ou editar um recurso (JSON)
+
+*/
+
+
+app.get('/projects', (request, response) => {
+    const { title, owner } = request.query
+
+    console.log(title)
+    console.log(owner)
+
+    return response.json([
+        'projeto 1',
+        'projeto 2',
+    ]);
+});
+
+app.post('/projects', (request, response) =>{
+    const {title, owner} = request.body
+    
+    console.log(title)
+    console.log(owner)
+
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3'
+    ])
+})
+
+app.put('/projects/:id', (request, response) =>{
+    const { id } = request.params
+    
+    console.log(id)
+
+    return response.json([
+        'Projeto 4',
+        'Projeto 2',
+        'Projeto 3'
+    ])
+})
+
+app.delete('/projects/:id', (request, response) =>{
+    return response.json([
+        'Projeto 2',
+        'Projeto 3'
+    ])
+})
+
+app.listen(3333, () => {
+    console.log('🚀 Back-end started!')
+});
+
