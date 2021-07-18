4cf7c0140da3044fc8f7d7266b15eacb602564fa
santiagoidu <sr_santiago@icloud.com>
2021-07-18 17:08:23 -0300

    front-end-ReactJS

--- 01-Frontend-com-ReactJS/src/index.js
+++ 01-Frontend-com-ReactJS/src/index.js
@@ -1,5 +1,8 @@
-const soma = (a, b) => {
-    return a + b
-}
+import React from 'react'
+import { render } from 'react-dom'
 
-console.log(soma(1, 3))
+import App from './App'
+
+// JSX  html dentro do JavaScript (javascript XML)
+
+render(<App />, document.getElementById('app'));
